14b8ab4f453551277dd403d430e317cf5be48763
ohotnikov.ivan <ohotnikov.ivan@e-queo.net>
2020-12-03 15:46:05 +0400

    last fix

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var dateFormat = require('dateformat');
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'chat',
+  password : '123123',
+  database : 'support_chat'
+});
+connection.connect();
+
+process.env.TZ = 'Europe/Volgograd';
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/layouts/index.html');
@@ -10,45 +22,75 @@ app.get('/manager', (req, res) => {
 });
 var users = [];
 io.on('connection', (socket) => {
-    socket.on('connect-user', function(room) {
+    socket.on('connect-user', function(room, params) {
         socket.join(room);
-        var usersObj = io.sockets.adapter.rooms.get('users');
-        if(usersObj)
-        {
-            var clients = Array.from(usersObj);
-            socket.to('manager').emit('manager-update', clients);
-        }else{
-            socket.to('manager').emit('manager-update', []);
+        if(room=="manager"){
+        } else{
+            var usersObj = io.sockets.adapter.rooms.get('users');
+            if(usersObj)
+            {
+                var clients = Array.from(usersObj);
+                socket.to('manager').emit('manager-update', clients);
+            }else{
+                socket.to('manager').emit('manager-update', []);
+            }
+          connection.query('SELECT * FROM users WHERE id = \''+socket.id+'\'', function (error, results, fields) {
+            if (!results[0]){
+              connection.query("INSERT INTO users SET online=1," +
+                                " id='"+socket.id+
+                                "', fio='"+params.fio+
+                                "', email='"+params.email+
+                                "', school='"+params.school+
+                                "', city='"+params.city+"'");
+            }
+          });
         }
     });
     socket.on('sendUserMessage', function (messageData) {
+        connection.query("INSERT INTO messages SET recipient='"+
+            messageData.user+
+            "', sender='manager', message='"+
+            messageData.message+"', date='"+
+            dateFormat(new Date(), 'h:MM:ss    |    d.m.yyyy')+
+            "'");
         socket.emit('messageFromUser',
             {
                 "userId": messageData.user,
                 "name": "Модератор",
-                "message": messageData.message
+                "message": messageData.message,
+                "date": dateFormat(new Date(), 'h:MM:ss    |    d.m.yyyy')
             });
         socket.to(messageData.user).emit('chat message',
             {
                 "name": "Модератор",
-                "message": messageData.message
+                "message": messageData.message,
+                "date": dateFormat(new Date(), 'h:MM:ss    |    d.m.yyyy')
             });
     });
     socket.on('chat message', function (messageData){
+        connection.query("INSERT INTO messages SET sender='"+socket.id+
+            "', recipient='manager', message='"+
+            messageData.message+"', date='"+
+            dateFormat(new Date(), 'h:MM:ss    |    d.m.yyyy')+
+            "'");
         socket.emit('chat message',
             {
                 "name": messageData.name,
-                "message": messageData.message
+                "message": messageData.message,
+                "date": dateFormat(new Date(), 'h:MM:ss    |    d.m.yyyy')
             });
         socket.to('manager').emit('messageFromUser',
             {
                 "userId": socket.id,
                 "name": socket.id,
-                "message": messageData.message
+                "message": messageData.message,
+                "date": dateFormat(new Date(), 'h:MM:ss    |    d.m.yyyy')
             });
     })
     socket.on('disconnect', () => {
         var userId = socket.id;
+        connection.query("UPDATE users SET online=0 " +
+                          "WHERE id = '"+userId+"'");
         var usersObj = io.sockets.adapter.rooms.get('users');
         if(usersObj)
         {
